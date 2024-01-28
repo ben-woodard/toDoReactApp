ea058ed96ec0969d16efd8103f3ea27f5e2feec2
Ben Woodard <143208459+ben-woodard@users.noreply.github.com>
2024-01-28 14:47:10 -0500

    initial commit
    created react app todo.  created header.js and footer.js

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>&copy; Copyright {new Date().getFullYear()}</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  doSomething();
+  return (
+    <div>
+      <h3>To Do App:</h3>
+    </div>
+  );
+};
+
+function doSomething(){
+  console.log("I'm doing something");
+}
+
+export default Header;
